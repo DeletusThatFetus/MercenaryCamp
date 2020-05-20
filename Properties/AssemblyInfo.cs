c6c0b815f8589787bd087a13ab16b5df33f78e1e
djhaxton1 <djhaxton@mtu.edu>
2020-05-20 17:44:48 -0600

    Initial Push with basic directory structure and configuration information

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+/// \copyright
+/// This file is part of MercenaryCamp (Mount & Blade II : Bannerlords Mod)
+///
+/// The MercenaryCamp Module is free software: you can redistribute it and/or
+/// modify it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// MercenaryCamp Module is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with MercenaryCamp.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// /purpose
+/// AssemblyInfo.cs contains information about your assembly, like name,
+/// description, version, etc. If you delete it, your assembly will be compiled
+/// with no information.
+///
+////////////////////////////////////////////////////////////////////////////////
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MercenaryCamp")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
